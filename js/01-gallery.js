424b1c39215c813b187a5d7b6a0626c0f33bb47e
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-04-27 23:22:06 +0300

    add close modal window by pressing Escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryList = document.querySelector('.gallery');
 const markup = galleryItems
-.map(({preview, original, description }, index) =>
+.map(({preview, original, description }) =>
  `<li data-source="${original}" class="js-card">
  <a href="${original}">
  <img src="${preview}" alt="${description}" class="js-target">
@@ -12,7 +12,6 @@ const markup = galleryItems
 .join("");
 
 galleryList.insertAdjacentHTML("beforeend", markup);
-
 
 galleryList.addEventListener("click", onClick);
 
@@ -24,14 +23,24 @@ function onClick(evt){
 
   const currentCard = evt.target.closest(".js-card");
 
-
-  console.dir(currentCard.querySelector("a > img").alt);
-
   const instance = basicLightbox.create(`
     <img src="${currentCard.dataset.source}"
       alt="${currentCard.querySelector("a > img").alt}"
       class="js-target"
-    >`);
-  instance.show()
+    >`, {
+      onShow: () => {
+        document.addEventListener('keydown', onEscape);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', onEscape);
+      }
+    });
 
+  instance.show();
+
+  function onEscape(e){
+   if(e.key === "Escape") {
+      instance.close();
+   }
+  }
 }
